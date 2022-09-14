0f99a14952f060a6fcc78525dc85da8709f430d9
Alireza <mrarshax@gmail.com>
2022-09-14 23:59:18 +0430

    Integer Modifiers

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 
 int main()
 {
-    int Zero{}; // Initializes to zero (by default)
-    std::cout << Zero << std::endl; // Output: 0
+    unsigned int One(1);
+    signed int NagativeOne(1);
 
-    int One(1);
-    std::cout << One << std::endl; // Output: 1
+    short ShortVar(10);
+    short int ShortIntVar(10);
+    unsigned short int UnsignedShotInt(10);
 
-    int number(1.2); // Safe way
-    // int number2{1.2}; Compiler error: narrowing conversion of '1.2e+0' from 'double' to 'int'
-    std::cout << number << std::endl; // Output: 1, The decimal part will be removed
+    long int LongInt(10);
+    unsigned long int UnsignedLongInt(10);
 
-    // -----------------------------------------
-
-    std::cout << "Size of int variable: " << sizeof(One) << std::endl;
+    std::cout << sizeof(One) << std::endl;
+    std::cout << sizeof(NagativeOne) << std::endl;
+    std::cout << sizeof(ShortVar) << std::endl;
+    std::cout << sizeof(ShortIntVar) << std::endl;
+    std::cout << sizeof(UnsignedShotInt) << std::endl;
+    std::cout << sizeof(LongInt) << std::endl;
+    std::cout << sizeof(UnsignedLongInt) << std::endl;
 }

a76f9cd75f97a3cc8ff04ed72f18cc1cc9a52845
Alireza <mrarshax@gmail.com>
2022-09-15 18:26:40 +0430

    Numeric Limits

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
-    bool t (true);
-    //t (false); Error
-    //t {false}; Error
-    t = false;
+    std::cout << "Short Min " << std::numeric_limits<short>::min()
+        << " Short Max " << std::numeric_limits<short>::max() << std::endl; 
+
+    std::cout << "Int Min " << std::numeric_limits<int>::min()
+        << " Int Max " << std::numeric_limits<int>::max() << std::endl; 
+
+    std::cout << "Unsigned Int Min " << std::numeric_limits<unsigned int>::min()
+        << " Unsigned Int Max " << std::numeric_limits<unsigned int>::max() << std::endl; 
+
+    std::cout << "Unsigned Short Int Min " << std::numeric_limits<unsigned short int>::min()
+        << " Unsigned Short Int Max " << std::numeric_limits<unsigned short int>::max() << std::endl; 
 }

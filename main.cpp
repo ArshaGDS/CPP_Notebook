d60fafd5bfcbd25fd856b5a1c511f3e2097ec991
Alireza <mrarshax@gmail.com>
2022-09-15 14:23:44 +0430

    Characters And Text

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 
 int main()
 {
-    char A ('A');
-    char B {'B'};
-    char APrime (65);
+    auto Var1 (10);    // integer
+    auto Var2 (10.1f); // float
+    auto Var3 (10.1);  // double
+    auto Var4 (10l);   // long
+    auto Var5 (10ul);  // unsigned long
+    auto Var6 (10ll);  // long long
+    auto Var7 (true);  // boolean
+    auto Var8 ("true");// string
+    auto Var9 ('t');   // char
 
-    std::cout << "Size of char variable: " << sizeof(char) << std::endl;
-    std::cout << A << " " << B << " " << APrime << std::endl;
-    std::cout << static_cast<int>(APrime) << std::endl;
+    std::cout << sizeof(Var1) << std::endl;
+    std::cout << sizeof(Var2) << std::endl;
+    std::cout << sizeof(Var3) << std::endl;
+    std::cout << sizeof(Var4) << std::endl;
+    std::cout << sizeof(Var5) << std::endl;
+    std::cout << sizeof(Var6) << std::endl;
+    std::cout << sizeof(Var7) << std::endl;
+    std::cout << sizeof(Var8) << std::endl;
+    std::cout << sizeof(Var9) << std::endl;
 }

c4925bd2ebc0dbda3420406f9186fb6565a92276
Alireza <mrarshax@gmail.com>
2022-09-15 14:06:46 +0430

    Characters And Text

--- main.cpp
+++ main.cpp
@@ -2,17 +2,11 @@
 
 int main()
 {
-    bool RedLight (true);
-    bool GreenLight (false);
+    char A ('A');
+    char B {'B'};
+    char APrime (65);
 
-    if (RedLight)
-    {
-        std::cout << "STOP!" << std::endl;
-    }
-
-    std::cout << "Green light is: " << GreenLight << std::endl;
-    std::cout << std::boolalpha; // Forces the output format to true / false
-    std::cout << "Red light is: " << RedLight << std::endl;
-
-    std::cout << "Size of boolean: " << sizeof(bool) << std::endl;
+    std::cout << "Size of char variable: " << sizeof(char) << std::endl;
+    std::cout << A << " " << B << " " << APrime << std::endl;
+    std::cout << static_cast<int>(APrime) << std::endl;
 }

ff20222372b1a79d1a5e5fa2c5ebc9929bb5a4c8
Alireza <mrarshax@gmail.com>
2022-09-16 17:44:36 +0430

    const, constexpr, constinit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip>
+
+// Can modify and initialized in compile time
+constinit int num2(5);
 
 int main()
 {
-    double DoubleNumber (7.7);
+    // Can't modify
+    const int num1(15);
+    const float PI(3.141516);
+    // Can't modify and initialized in compile time
+    constexpr int num3(3);
 
-    std::cout << "Rounded to floor: " << std::floor(DoubleNumber) << std::endl;
-    std::cout << "Rounded to ceil: " << std::ceil(DoubleNumber) << std::endl;
-    std::cout << "-----------------------------------" << std::endl;
+    std::cout << std::fixed; // show fix float value
+    std::cout << PI << std::endl;    
+    std::cout << num2 << std::endl;    
+    num2 = PI;
+    std::cout << num2 << std::endl;    
 
-    int IntNumber (-5000);
-    
-    std::cout << "abs of DoubleNumber: " << std::abs(DoubleNumber) << std::endl;
-    std::cout << "abs of IntNumber: " << std::abs(IntNumber) << std::endl;
-    std::cout << "-----------------------------------" << std::endl;
-
-    // exp: f(x) = e ^ x, where e = 2.71828
-    std::cout << "The exponential of 10 is: " << std::exp(10) << std::endl;
-    std::cout << "-----------------------------------" << std::endl;
-
-    std::cout << "3 ^ 4 is: " << std::pow(3, 4) << std::endl;
-    std::cout << "-----------------------------------" << std::endl;
 }

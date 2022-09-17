48c3537e9a34abd11fa8fc277e55ae352642c02a
Alireza <mrarshax@gmail.com>
2022-09-18 01:08:46 +0430

    Shift Operators

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 #include <bitset>
 
 int main()
-{              
-    unsigned short int data {8};
-    std::cout << std::bitset<16>(data) << std::endl;
+{                  
+    short int Number = 3;
+
+    // Shifting right, divides by 2^n
+    short int Right = static_cast<short int>(Number >> 1);
+    std::cout << "Right (Dec): " << Right << " (Bin): " << std::bitset<16>(Right) << std::endl;
+
+    // Shifting left, multiplies by 2^n
+    short int Left = static_cast<short int>(Number << 1);
+    std::cout << "Left  (Dec): " << Left << " (Bin): " << std::bitset<16>(Left) << std::endl;
 
     return 0;
 }

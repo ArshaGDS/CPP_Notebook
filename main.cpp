e19563cae923ec4f9c227b0bef6b6fe0bbb0c13b
Alireza <mrarshax@gmail.com>
2022-09-17 19:29:09 +0430

    Setbit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
+#include <bitset>
 
 int main()
-{
-    double num1 = 1.1, num2 = 2.2;
-    auto sum = static_cast<int>(num1 + num2);
-    std::cout << "Sum: " << sum << " Size: " << sizeof(sum) << std::endl;
+{              
+    unsigned short int data {8};
+    std::cout << std::bitset<16>(data) << std::endl;
 
-    //----------------------------------
-
-    // not safe and old style (C style) cast
-    double PI {3.14};
-    int IntPI { (int)PI };
-    std::cout << "Int PI: " << IntPI << " Size: " << sizeof(IntPI) << std::endl;
+    return 0;
 }

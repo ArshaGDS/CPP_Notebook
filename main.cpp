613b939969d813fb073fccc93901d998245f5a82
Alireza <mrarshax@gmail.com>
2022-09-19 01:53:04 +0430

    Switch

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <iomanip>
-#include <bitset>
-
-constexpr unsigned short int WIDTH{20};
-
-constexpr unsigned int red_mask   {0xFF000000};
-constexpr unsigned int green_mask {0x00FF0000};
-constexpr unsigned int blue_mask  {0x0000FF00};
-constexpr unsigned int alpha_mask {0x000000FF};
-
-void SetColor(unsigned int Color)
-{
-    std::cout << std::left;
-    std::cout << std::setw(WIDTH) << ((Color & red_mask) >> 24) << std::endl; 
-    std::cout << std::setw(WIDTH) << ((Color & green_mask) >> 16) << std::endl; 
-    std::cout << std::setw(WIDTH) << ((Color & blue_mask) >> 8) << std::endl; 
-    std::cout << std::setw(WIDTH) << ((Color & alpha_mask) >> 0) << std::endl; 
-}
 
 int main()
-{            
-   SetColor(red_mask | alpha_mask);
+{          
+    unsigned short int Score{18};  
+    switch(Score)
+    {
+        case 20 : {
+            std::cout << "Good job" << std::endl;
+        }
+        break;
+        case 15 : { std::cout << "Not bad" << std::endl; }
+        break;
+        case 13 :
+        case 12 : {}
+        case 11 :  std::cout << "Ridi" << std::endl; 
+        break;
+        default : {
+            std::cout << "Khode khari" << std::endl;
+        }
+        break;
+    }
 }

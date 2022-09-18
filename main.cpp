854bc6f00d3e24ec6b49f972c749ba0b60738511
Alireza <mrarshax@gmail.com>
2022-09-18 15:04:08 +0430

    Compound Bitwise and Assignment Operators

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <bitset>
 
 int main()
-{                  
-    unsigned short int num1{2};
-    unsigned short int num2{2};
-    constexpr unsigned short int WIDTH{10};
+{            
+    constexpr unsigned short int WIDTH{21}; 
 
-    std::cout << "Num1: " << std::setw(WIDTH) << std::bitset<sizeof(num1) * 8>(num1) << std::endl;
-    std::cout << "Num2: " << std::setw(WIDTH) << std::bitset<sizeof(num2) * 8>(num2) << std::endl;
-    std::cout << "Sum : " << std::setw(WIDTH) << std::bitset<sizeof(num2) * 8>(num1 ^ num2) << std::endl;
+    unsigned short int var{3}; // 0011
+    std::cout << std::left;
+    std::cout << std::setw(WIDTH) << "original var: " << std::setw(WIDTH) << std::bitset<sizeof(var) * 8>(var) << std::endl;
+    std::cout << std::setw(WIDTH) << "shifting left var: " << std::setw(WIDTH) << std::bitset<sizeof(var) * 8>(var << 1) << std::endl;
+    std::cout << std::setw(WIDTH) << "after shift var: " << std::setw(WIDTH) << std::bitset<sizeof(var) * 8>(var) << std::endl;
+    std::cout << std::setw(WIDTH) << "shift and store var: " << std::setw(WIDTH) << std::bitset<sizeof(var) * 8>(var <<= 1) << std::endl;
+    std::cout << std::setw(WIDTH) << "var after shift: " << std::setw(WIDTH) << std::bitset<sizeof(var) * 8>(var) << std::endl;
 }

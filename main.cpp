11a5c7f0fc34e10e2f9dcce74e8899aa0722a060
Alireza <mrarshax@gmail.com>
2022-09-20 16:56:15 +0430

    if and switch with initializer

--- main.cpp
+++ main.cpp
@@ -1,46 +1,29 @@
 #include <iostream>
-#include <string>
 
 int main()
 {          
-    std::string SoldierCharacter{"Soldier"};
-    unsigned short int SoldierHealth{100};
+    constexpr unsigned short int Pen{10};
+    constexpr unsigned short int Eraser{11};
+    constexpr unsigned short int Pencil{12};
 
-    std::string EnemyCharacter{"Enemy"};
-    unsigned short int EnemyHealth{200};
+    if(float Number{1.2f}; Pen < Pencil)
+    {
+        std::cout << "Float number is: " << Number << std::endl;
+    }
 
-    std::string CharacterType{SoldierCharacter};
-    unsigned short int MaximumHealth{ CharacterType == SoldierCharacter ? SoldierHealth : EnemyHealth };
-    unsigned short int Health{16};
-    unsigned short int AppliedDamage{10};
-    unsigned short int Medkit{150};
-
-    // Take damage
-    Health -= AppliedDamage > Health ? Health : AppliedDamage;
-    std::cout << "Take damagem" << std::endl << "Current health: " << Health << std::endl;
-    // Take damage
-    Health -= AppliedDamage > Health ? Health : AppliedDamage;
-    std::cout << "Take damagem" << std::endl << "Current health: " << Health << std::endl;
-    // Healing
-    Health += Medkit > MaximumHealth ? MaximumHealth : Medkit;
-    std::cout << "Healing..." << std::endl << "Current health: " << Health << std::endl;
-    std::cout << std::endl;
-
-    //---------------------------------------------------------------------------------------
-
-    int NormalInt{1};       // 4 Bytes
-    short int ShortInt{1};  // 2 Bytes
-    long int LongInt{1};    // 8 Bytes
-
-    auto val1 { 1 > 2 ? ShortInt : NormalInt };
-    std::cout << sizeof(val1) << std::endl; // 4 Bytes
-
-    auto val2 { 1 < 2 ? ShortInt : NormalInt };
-    std::cout << sizeof(val2) << std::endl; // 2 Bytes
-
-    auto val3 { 1 > 2 ? NormalInt : LongInt };
-    std::cout << sizeof(val3) << std::endl; // 8 Bytes
-
-    auto val4 { 1 < 2 ? NormalInt : LongInt };
-    std::cout << sizeof(val4) << std::endl; // 2 Bytes
+    constexpr unsigned short int Tool{Eraser};
+    switch (short int Number{-15}; Tool)
+    {
+    case Pen:
+        /* code */
+        break;
+    case Eraser:
+        /* code */
+        break;
+    case Pencil:
+        /* code */
+        break;
+    default:
+        break;
+    }
 }

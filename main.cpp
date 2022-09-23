1aea6486a64402f3dcd1594b33c7b30a4083aa74
Alireza <mrarshax@gmail.com>
2022-09-23 21:42:42 +0330

    Range Based For Loop

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 
 int main()
 {              
-    int number1{10};
-    int number2{20};
-    int number3{25};
-    int result{ (number1 *= ++number2, number3 - number2) };
-    std::cout << number1 << std::endl; // 210
-    std::cout << number2 << std::endl; // 21
-    std::cout << number3 << std::endl; // 25
-    std::cout << result  << std::endl; // 4 -> number3 - number2 -> 25 - 21
+    int array[]{0,1,2,3,4,5,6,7,8,9};
+    for (int index : array)
+    {
+        std::cout << "Index: " << index << std::endl;
+    }    
 }

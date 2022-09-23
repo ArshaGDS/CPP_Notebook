705cc2e37d8469ca8992784f2919f685bf73ad1d
Alireza <mrarshax@gmail.com>
2022-09-23 13:07:42 +0330

    For Loop  Multiple Declarations

--- main.cpp
+++ main.cpp
@@ -2,31 +2,9 @@
 
 int main()
 {          
-    for (/* Initialize with zero */unsigned int i{} ; i < /*Hard code*/10; i++)
+    for (size_t x{1}, y{5}, z{0} ; y > 0 ; x++, y--, z += 2)
     {
-        std::cout << "I Love C++" << std::endl;
-    }
-    std::cout << "-------------------" << std::endl;
-
-    //------------------------------------------------------
-
-    /*
-        size_t size is different in compilers
-    */
-
-    for (size_t i{} ; i < 10; i++)
-    {
-        std::cout << "I Love C++" << std::endl;
-    }
-    std::cout << "-------------------" << std::endl;
-
-    //------------------------------------------------------
-
-    // Without hard code
-    const unsigned int COUNT{10};
-    for (unsigned int i{} ; i < COUNT; i++)
-    {
-        std::cout << "I Love C++" << std::endl;
+        std::cout << "X: " << x << " Y: " << y << " z: " << z << std::endl;
     }
     std::cout << "-------------------" << std::endl;
 }

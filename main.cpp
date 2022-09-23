3a1c12903852b03dcc1f2f5413b40679b15b4607
Alireza <mrarshax@gmail.com>
2022-09-23 20:51:17 +0330

    Comma Operator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 int main()
-{          
-    for (size_t x{1}, y{5}, z{0} ; y > 0 ; x++, y--, z += 2)
-    {
-        std::cout << "X: " << x << " Y: " << y << " z: " << z << std::endl;
-    }
-    std::cout << "-------------------" << std::endl;
+{              
+    int number1{10};
+    int number2{20};
+    int number3{25};
+    int result{ (number1 *= ++number2, number3 - number2) };
+    std::cout << number1 << std::endl; // 210
+    std::cout << number2 << std::endl; // 21
+    std::cout << number3 << std::endl; // 25
+    std::cout << result  << std::endl; // 4 -> number3 - number2 -> 25 - 21
 }

f351cb447a484762e5ad9313b423d4d68f22f840
Alireza <mrarshax@gmail.com>
2022-09-24 20:21:35 +0330

    Int Array

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {              
-    int array[]{0,1,2,3,4,5,6,7,8,9};
-    for (int index : array)
+    int array[] {1, 2, 3, 4, 5, 6};
+
+    std::cout << "Index 0: " << array[0] << std::endl;
+    std::cout << "Index 1: " << array[1] << std::endl;
+    std::cout << "Index 2: " << array[2] << std::endl;
+    array[2] = 5;
+    std::cout << "Index 2: " << array[2] << std::endl;
+
+    unsigned short int WIDTH{30};
+    std::cout << std::left;
+    std::cout << std::setw(WIDTH) << "Size of Index 0: " << std::setw(WIDTH) << sizeof(array[0]) << std::endl;
+    std::cout << std::setw(WIDTH) << "Size of array: " << std::setw(WIDTH) << sizeof(array) << std::endl;
+    std::cout << std::setw(WIDTH) << "Item count: " << std::setw(WIDTH) << (sizeof(array) / sizeof(array[0])) << std::endl;
+    std::cout << std::setw(WIDTH) << "Item count with std library: "<< std::setw(WIDTH)  << std::size(array) << std::endl;
+
+    for(auto index : array)
     {
-        std::cout << "Index: " << index << std::endl;
-    }    
+        std:: cout << index;
+    }
+
+    std:: cout << std::endl;
+
+    for (unsigned short int index = 0; index < std::size(array); index++)
+    {
+        std:: cout << array[index];
+    }
+    
 }

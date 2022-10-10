844ec9d8eb7edb8d59426c1bd89358e5424653d4
Alireza <mrarshax@gmail.com>
2022-10-10 14:37:23 +0330

    Pointers and Arrays

--- main.cpp
+++ main.cpp
@@ -1,28 +1,13 @@
 #include <iostream>
-#include <iomanip>
 
 int main()
 {              
-    // Wrong way, because the answer is always the same
-    std::cout << "RAND_MAX: " << RAND_MAX << std::endl; // 32767 in GCC compiler and 64bit system
+    int scores[2] {1, 4};
+    int *scorePtr {scores};
 
-    int randomNumber1 = std::rand(); // between 0 and RAND_MAX
-    std::cout << "randomNumber1: " << randomNumber1 << std::endl;
-
-    int randomNumber2 = std::rand() % 11; // between 0 and 10
-    std::cout << "randomNumber2: " << randomNumber2 << std::endl;
-
-    int randomNumber3 = (std::rand() % 11) + 1; // between 1 and 10
-    std::cout << "randomNumber3: " << randomNumber3 << std::endl;
-
-    //-----------------------------------------------------------------
-
-    
-    std::srand(std::time(0));
-    for (size_t i = 0; i < 10; i++)
-    {
-        std::cout << std::rand() % 20 << " . ";
-    }
-    std::cout << std::endl;
-
+    std::cout << "*scores " << *scores << std::endl;
+    std::cout << "scores " << scores << std::endl;
+    std::cout << "scores[0] " << scores[0] << std::endl;
+    std::cout << "scorePtr " << scorePtr << std::endl;
+    std::cout << "scorePtr[1] " << scorePtr[1] << std::endl;
 }

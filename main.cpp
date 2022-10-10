a303f21192815da81910f3215cc3195c181ab1b3
Alireza <mrarshax@gmail.com>
2022-10-10 19:21:40 +0330

    Pointer Arithmetic Navigation V2

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 
 int main()
 {              
-    int array[5];
+    int array[10];
     std::srand(time(0));
     for (int index = 0; index < std::size(array); index++)
     {
         array[index] = std::rand() % 11;
+        //array[index] = index;
+        std::cout << array[index] << (index + 1 < std::size(array) ? " . " : "");
     }
+    std::cout << std::endl;
     
-    for (int index = 0; index < std::size(array); index++)
-    {
-        std::cout << *(array + index) << std::endl;
-    }    
+    int* arrayPtr {array};  
+    std::cout << arrayPtr << ": " << *arrayPtr << std::endl;
+    std::cout << (arrayPtr + 8) << ": " << *(arrayPtr + 8)  << std::endl;
 }

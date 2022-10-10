b9ce65f4c32e2b9e706625c94af8ad612a5fa142
Alireza <mrarshax@gmail.com>
2022-10-10 18:37:25 +0330

    Pointer Arithmetic  Navigation

--- main.cpp
+++ main.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
 
-void PrintArray(int Array[])
-{        
-    for (int index{}; index < 5; index++)
-    {
-        std::cout << Array[index] << " . ";
-    }
-    std::cout << std::endl;    
-}
-
 int main()
 {              
-    int array1[5] {1, 2, 3, 4, 5};
-    int array2[5] {6, 7, 8, 9, 0};
-
-    int* arr1Ptr {array1};
-    int* arr2Ptr {array2};
-    int* temp;
-
-    PrintArray(arr1Ptr);
-    PrintArray(arr2Ptr);
-
-    temp = arr1Ptr;
-    arr1Ptr = arr2Ptr;
-    arr2Ptr = temp;
-
-    PrintArray(arr1Ptr);
-    PrintArray(arr2Ptr);
+    int array[5];
+    std::srand(time(0));
+    for (int index = 0; index < std::size(array); index++)
+    {
+        array[index] = std::rand() % 11;
+    }
+    
+    for (int index = 0; index < std::size(array); index++)
+    {
+        std::cout << *(array + index) << std::endl;
+    }    
 }

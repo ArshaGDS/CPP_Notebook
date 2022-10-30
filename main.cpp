c84bed6b7c4c514bf0ad136101b04ce9360c1543
Alireza <mrarshax@gmail.com>
2022-10-31 00:45:26 +0330

    C-string manipulation

--- main.cpp
+++ main.cpp
@@ -1,47 +1,20 @@
 #include <iostream>
 #include <cctype>
-#define Log(x) std::cout << (x) << std::endl
+#include <cstring>
 
 int main()
 {
-    // Check is character is alphanumeric
-    // If char is A-Z, a-z, 0-9 return none zero number
-    // If Char is !@#$%^&*()=-+_ return zero
-    std::cout << "isalnum" << std::endl;
-    std::cout << std::isalnum('4') << std::endl;
-    std::cout << std::isalnum('f') << std::endl;
-    std::cout << std::isalnum('D') << std::endl;
-    std::cout << std::isalnum('=') << std::endl;
-    std::cout << std::isalnum('@') << std::endl;
-    std::cout << std::isalnum('%') << std::endl;
-    //============================================
-    // Check if character is alphbetic
-    // If char is A-Z, a-z, return none zero number else return zero
-    std::cout << "isalpha" << std::endl;
-    std::cout << std::isalpha('4') << std::endl;
-    std::cout << std::isalpha('f') << std::endl;
-    std::cout << std::isalpha('D') << std::endl;
-    //============================================
-    // Check if character is blank
-    // If char is black, return none zero number else return zero
-    std::cout << "isblank" << std::endl;
-    std::cout << std::isblank(' ') << std::endl;
-    std::cout << std::isblank('f') << std::endl;
-    std::cout << std::isblank('D') << std::endl;
-    //============================================
-    // Check if character is lowercase / uppercase
-    // If char is lowercase / uppercase, return none zero number else return zero
-    std::cout << "islower" << std::endl;
-    std::cout << std::islower('f') << std::endl;
-    std::cout << std::islower('F') << std::endl;
-    std::cout << "isupper" << std::endl;
-    std::cout << std::isupper('f') << std::endl;
-    std::cout << std::isupper('F') << std::endl;
-    //============================================
-    // Check if character is digit
-    // If char is digit, return none zero number else return zero
-    std::cout << "isidigit" << std::endl;
-    std::cout << std::isdigit('3') << std::endl;
-    std::cout << std::isdigit('F') << std::endl;
-    //============================================
+    const char* MessageStr {"Hello World :D"};
+    const char* MessageStr2{"Hello World :D"};
+    const char* MessageStr3{"Hello World"};
+
+    std::cout << "std::strcmp(MessageStr, MessageStr2)      : " << std::strcmp(MessageStr, MessageStr2) << std::endl;
+    std::cout << "std::strcmp(MessageStr, MessageStr3)      : " << std::strcmp(MessageStr, MessageStr3) << std::endl;
+
+    size_t n{3};
+    std::cout << "std::strncmp(MessageStr2, MessageStr3, n) : " << std::strncmp(MessageStr2, MessageStr3, n) << std::endl;
+
+    const char* result{MessageStr};
+    char target{'l'};
+    std::cout << std::strchr(MessageStr, target) << std::endl;
 }

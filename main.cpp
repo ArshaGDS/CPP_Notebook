b3043b8da7fb0abf5e5ab1c2b44821fa1a0d60d7
Alireza <mrarshax@gmail.com>
2022-11-01 23:54:08 +0330

    Concatenating stdstring V2

--- main.cpp
+++ main.cpp
@@ -3,23 +3,15 @@
 
 int main()
 {
-    std::string Str1{"Hello"};
-    std::string Str2{"World"};
+    std::string Message;
+    {
+        using namespace std::string_literals;
+        Message = "Heyyy it's old school string in modern std string"s + " :D ";
+    }
 
-    std::string Message1{Str1 + Str2};
-    std::string Message2{Str1 + " " + Str2};
-    std::string Message3{Str1.append(" " + Str2)};
-    std::string Message4{"Hello" " " "World"};
-
-    std::string Text1{"BlaBlaBlaBla"};
-    std::string Text2{Text1.append(5, '!')};
-    std::string Text3{Str1.append(Text2, Text1.length() - 5, 2)};
-
-    std::cout << Message1 << std::endl;
+    std::string Message2{"UE" + 5};
+    std::string Message3{"UE " + std::to_string(5)};
+    std::cout << Message  << std::endl;
     std::cout << Message2 << std::endl;
     std::cout << Message3 << std::endl;
-    std::cout << Message4 << std::endl;
-
-    std::cout << Text2 << std::endl;
-    std::cout << Text3 << std::endl;
 }

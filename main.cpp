7729c50a309a49eca97fa36fbb9417aa1ce3f32d
Alireza <mrarshax@gmail.com>
2022-11-02 16:32:08 +0330

    stdstring size and capacity

--- main.cpp
+++ main.cpp
@@ -3,39 +3,25 @@
 
 int main()
 {
-    std::string Str{"Arsha"};
-    std::cout << Str.size() << std::endl;
-    std::cout << Str.length() << std::endl;
+    std::string Empty;
+    std::string Initialized{};
+    std::string Str{"Str"};
+    std::string Str2{"Str"};
+    std::string Str3{"StringStringStringStringStringStringStringString"};
 
-    Str.at(2) = 'S';
-    std::cout << "The third char of Str: " << Str.at(2) << std::endl;
+    std::cout << std::boolalpha << Empty.empty() << std::endl;
+    std::cout << std::boolalpha << Initialized.empty() << std::endl;
+    std::cout << std::boolalpha << Str.empty() << std::endl;
 
-    //---------------------------------------------------------
+    std::cout << Empty.capacity() << std::endl;
+    std::cout << Initialized.capacity() << std::endl;
+    std::cout << Str.capacity() << std::endl;
+    std::cout << Str2.capacity() << std::endl; 
+    std::cout << Str3.capacity() << std::endl;
 
-    char Front = Str.front();
-    char Back  = Str.back();
-
-    char& Front_Ref = Str.front();
-    char& Back_Ref  = Str.back();
-
-    std::cout << "Front: " << Front << " Back: " << Back << " Front_Ref: " << Front_Ref << " Back_Ref: " << Back_Ref << std::endl;
-
-    Front = 'a';
-    Back  = 'A';
-    std::cout << "Str: " << Str << std::endl;
-
-    Front_Ref = 'a';
-    Back_Ref  = 'A';
-    std::cout << "Str: " << Str << std::endl;
-
-    std::cout << std::endl;
-
-    std::string Str2{"Hello World"};
-    std::string Str2_c_str{Str2.c_str()};
-    std::string Str2_data{Str2.data()};
-
-    std::cout << Str2.length() << std::endl;
-    std::cout << Str2_c_str.length() << std::endl;
-    std::cout << Str2_data.length() << std::endl;
-    std::cout << std::endl;
+    std::string Str4{"Str"};
+    Str4.reserve(100);
+    std::cout << Str4.capacity() << std::endl;
+    Str4.shrink_to_fit();
+    std::cout << Str4.capacity() << std::endl;
 }

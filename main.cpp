30fc7a3781b6d74b37d5dbd18153b7ffc08a4947
Alireza <mrarshax@gmail.com>
2022-11-06 21:46:31 +0330

    stdstring_view

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 
 int main()
 {
-    // Raw string litterals
-    std::string StrFileAddress { R"(D:\Videos\Tutorials\C++20)" };
-    std::string StrMessage { R"---(The message was "(Stay out of this!)")---" };
-    std::string StrMessage2 {
-        R"(
-        Multiline 
-        String!
-        )"
-    };
-    
-    std::cout << "Address: " << StrFileAddress << std::endl;
-    std::cout << StrMessage  << std::endl;
-    std::cout << StrMessage2 << std::endl;
+    std::string Str{"Hello World!"};
+    std::string_view StrView{Str};
+    std::string_view StrView2{StrView};
+
+    std::cout << "Str: " << Str << std::endl;
+    std::cout << "StrView: " << StrView << std::endl;
+
+    StrView.remove_prefix(5);
+    std::cout << StrView << std::endl;
+
+    StrView2.remove_suffix(6);
+    std::cout << StrView2 << std::endl;
 }

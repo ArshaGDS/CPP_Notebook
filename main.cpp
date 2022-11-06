0168c5ee69fd6fb9475c1b5a6777b603412cdeb1
Alireza <mrarshax@gmail.com>
2022-11-06 19:45:17 +0330

    Raw string litterals

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 
 int main()
 {
-    std::string StrNum1{"1"};
-    std::string StrNum2{"1.2f"};
-    std::string StrNum3{"1.23456789"};
-
-    int Num1 = std::stoi(StrNum1);
-    int Num2 = std::stof(StrNum2);
-    int Num3 = std::stod(StrNum3);
-
-    std::cout << "Num1: " << sizeof(Num1) << std::endl;
-    std::cout << "Num2: " << sizeof(Num2) << std::endl;
-    std::cout << "Num3: " << sizeof(Num3) << std::endl;
+    // Raw string litterals
+    std::string StrFileAddress { R"(D:\Videos\Tutorials\C++20)" };
+    std::string StrMessage { R"---(The message was "(Stay out of this!)")---" };
+    std::string StrMessage2 {
+        R"(
+        Multiline 
+        String!
+        )"
+    };
+    
+    std::cout << "Address: " << StrFileAddress << std::endl;
+    std::cout << StrMessage  << std::endl;
+    std::cout << StrMessage2 << std::endl;
 }

8e12996bcb6c56b9e9151dc25d16286f39c223a0
Alireza <mrarshax@gmail.com>
2022-11-14 17:14:58 +0330

    Type aliases

--- main.cpp
+++ main.cpp
@@ -1,37 +1,14 @@
 #include <iostream>
 #include <string>
 
-enum class Month 
-{
-    Jan, January   = Jan,
-    Feb, February  = Feb,
-    Mar, March     = Mar,
-    Apr, April     = Apr,
-    May,
-    Jun, June      = Jun,
-    Aug, August    = Aug,
-    Sep, September = Sep,
-    Oct, October   = Oct,
-    Nov, November  = Nov,
-    Dec, December  = Dec
-};
-
-std::string_view MonthToString(Month month)
-{
-    switch (month)
-    {
-        using enum Month;
-        case Jan : return "January";
-        case Feb : return "February";
-        case Mar : return "March";
-        case Apr : return "April";
-        // ...
-        default  : return "Invalid input";
-    }
-}
-
 int main()
-{
-    Month month {Month::Apr};
-    std::cout << MonthToString(month) << std::endl;
+{    
+    typedef unsigned int UInt; // Not recommended (Old way)
+    using HugeInt = unsigned long long int; // Recommended in modern C++
+
+    HugeInt HugeNume {18'446'744'073'709'551ull};
+    std::cout << HugeNume << std::endl;   
+
+    UInt uint {1234};
+    std::cout << uint << std::endl;   
 }

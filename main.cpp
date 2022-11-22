2ce5713565c17f5dd6286b4e1bfb62b3ff5a0a2e
Alireza <mrarshax@gmail.com>
2022-11-22 12:41:19 +0330

    Auto type deduction

--- main.cpp
+++ main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 
 int main()
 {    
-    typedef unsigned int UInt; // Not recommended (Old way)
-    using HugeInt = unsigned long long int; // Recommended in modern C++
+    int Num{123};
+    int NumRef{Num};
 
-    HugeInt HugeNume {18'446'744'073'709'551ull};
-    std::cout << HugeNume << std::endl;   
+    auto AutVar{Num};
+    auto AutVar2{NumRef};
 
-    UInt uint {1234};
-    std::cout << uint << std::endl;   
+    int WIDTH{15};
+    std::cout << std::left;
+    std::cout << std::setw(WIDTH) << "Num:"     << Num     << std::endl;
+    std::cout << std::setw(WIDTH) << "NumRef:"  << NumRef  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutVar:"  << AutVar  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutVar2:" << AutVar2 << std::endl;
+    
+    auto& AutRef{Num};
+    auto& AutRef2{NumRef};
+
+    std::cout << std::setw(WIDTH) << "AutRef:"  << AutRef  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutRef2:" << AutRef2 << std::endl;
+
+    std::cout << "###################################################" << std::endl;
+
+    AutVar++;
+    std::cout << std::setw(WIDTH) << "Num:"     << Num     << std::endl;
+    std::cout << std::setw(WIDTH) << "NumRef:"  << NumRef  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutVar:"  << AutVar  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutVar2:" << AutVar2 << std::endl;
+    std::cout << std::setw(WIDTH) << "AutRef:"  << AutRef  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutRef2:" << AutRef2 << std::endl;
+    AutVar--;
+
+    std::cout << "###################################################" << std::endl;
+
+    AutRef++;
+    std::cout << std::setw(WIDTH) << "Num:"     << Num     << std::endl;
+    std::cout << std::setw(WIDTH) << "NumRef:"  << NumRef  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutVar:"  << AutVar  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutVar2:" << AutVar2 << std::endl;
+    std::cout << std::setw(WIDTH) << "AutRef:"  << AutRef  << std::endl;
+    std::cout << std::setw(WIDTH) << "AutRef2:" << AutRef2 << std::endl;
+
+    std::cout << "###################################################" << std::endl;
 }

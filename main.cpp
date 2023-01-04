05a693fe67f530f2ebd4fa75ec59cba21904b5aa
Alireza <mrarshax@gmail.com>
2023-01-04 12:24:44 +0330

    Template with multiple parameters

--- main.cpp
+++ main.cpp
@@ -20,6 +20,13 @@ const char* Maximum<const char*>(const char* a, const char* b)
     return (std::strcmp(a, b) > 0 ? a : b);
 }
 
+// Multiple parameters
+template <typename ReturnType, typename T0, typename T1>
+ReturnType Sum(T0 a, T1 b)
+{
+    return a + b;
+}
+// Bad design!!!
 template <typename T0, typename T1>
 T0 Sum(T0 a, T1 b)
 {
@@ -39,7 +46,13 @@ int main()
     std::cout << Sum(a, d) << std::endl;
     std::cout << Sum<double>(a, b) << std::endl;
 
+    // Template specialization
     const char* A{"A"};
     const char* B{"B"};
     std::cout << Maximum(A, B) << std::endl;
+
+    // Multiple parameters
+    std::cout << Sum<float, int, int>(a, b) << std::endl;
+    std::cout << Sum<float, int, float>(a, c) << std::endl;
+    std::cout << Sum<int, int, float>(a, c) << std::endl;
 }

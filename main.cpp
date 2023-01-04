3314fb3a0f026357f4a7db089f85dc8c1c94b412
Alireza <mrarshax@gmail.com>
2023-01-04 22:05:01 +0330

    Named template parameters for kambdas

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
-auto func_add(auto A, auto B)
-{
-    return A + B;
-}
-
 int main()
 {
-    int Num1{5};
-    double Num2{10};
-    auto Result = func_add(Num1, Num2);
-    std::cout << "Size: " << sizeof(Result) << std::endl;
+    auto func = [](auto A, auto B) {
+        return A + B;
+    };
+
+    // C++20
+    auto funcV20 = []<typename T0, typename T1>(T0 A, T1 B) {
+        return A + B;
+    };
+
+    int a{20};
+    double b{20.5};
+
+    std::cout << "Sum: " << func(a, b) << std::endl;
+    std::cout << "Sum: " << funcV20(a, b) << std::endl;
 }

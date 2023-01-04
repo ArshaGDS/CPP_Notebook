9f8aa923721e5d7780f9bfb07c11162dad3b22ce
Alireza <mrarshax@gmail.com>
2023-01-04 17:39:20 +0330

    Template return type deduction with auto

--- main.cpp
+++ main.cpp
@@ -20,18 +20,26 @@ const char* Maximum<const char*>(const char* a, const char* b)
     return (std::strcmp(a, b) > 0 ? a : b);
 }
 
+// Template return type deduction with auto
+// --> Larger type is going to be deduced as return type <--
+template <typename T0, typename T1>
+auto Maximum(T0 a, T1 b)
+{
+    return (a > b) ? a : b;
+}
+
 // Multiple parameters
 template <typename ReturnType, typename T0, typename T1>
 ReturnType Sum(T0 a, T1 b)
 {
     return a + b;
 }
-// Bad design!!!
-template <typename T0, typename T1>
-T0 Sum(T0 a, T1 b)
-{
-    return a + b;
-}
+// Multiple parameters: Bad design!!!
+// template <typename T0, typename T1>
+// T0 Sum(T0 a, T1 b)
+// {
+//     return a + b;
+// }
 
 int main()
 {    
@@ -43,7 +51,7 @@ int main()
     std::cout << Maximum(&a, &b) << " " << *Maximum(&a, &b) << std::endl;
     std::cout << Maximum(&c, &d) << " " << *Maximum(&c, &d) << std::endl;
 
-    std::cout << Sum(a, d) << std::endl;
+    std::cout << Sum<int>(a, b) << std::endl;
     std::cout << Sum<double>(a, b) << std::endl;
 
     // Template specialization
@@ -52,7 +60,17 @@ int main()
     std::cout << Maximum(A, B) << std::endl;
 
     // Multiple parameters
-    std::cout << Sum<float, int, int>(a, b) << std::endl;
-    std::cout << Sum<float, int, float>(a, c) << std::endl;
-    std::cout << Sum<int, int, float>(a, c) << std::endl;
+    std::cout << Sum<double, int, int>(a, b) << std::endl;
+    std::cout << Sum<double, int, double>(a, c) << std::endl;
+    std::cout << Sum<int, int, double>(a, c) << std::endl;
+
+    // Template return type deduction with auto
+    {
+        auto SumResult { Maximum(a, c) };
+        std::cout << SumResult << " " << sizeof(SumResult) << std::endl;        
+    }
+    {
+        auto SumResult { Maximum(c, a) };
+        std::cout << SumResult << " " << sizeof(SumResult) << std::endl; 
+    }
 }

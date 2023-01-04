21931026a677f6a45baa3a32392c5cc964714fdd
Alireza <mrarshax@gmail.com>
2023-01-04 18:48:54 +0330

    Decltype auto

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 template <typename T0, typename T1>
-auto Maximum(T0 A, T1 B) -> decltype(( A > B) ? A : B); // Trailing return type
+decltype(auto) Maximum(T0 A, T1 B)
+{
+    return (A > B) ? A : B;
+}
 
 int main()
 {
@@ -9,10 +12,4 @@ int main()
     char c{10};
     auto Max1 = Maximum(c, i);
     std::cout << sizeof(Max1) << std::endl;
-}
-
-template <typename T0, typename T1>
-auto Maximum(T0 A, T1 B) -> decltype((A > B) ? A : B)
-{
-    return (A > B) ? A : B;
 }

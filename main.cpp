9e74fa1133fbe622210f82cd5569648e24c32836
Alireza <mrarshax@gmail.com>
2023-01-04 10:59:31 +0330

    Template specialization

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 template <typename T>
 T Maximum(T a, T b)
@@ -6,12 +7,18 @@ T Maximum(T a, T b)
     return (a > b) ? a : b;
 }
 
+// Template specialization
+template <>
+const char* Maximum<const char*>(const char* a, const char* b)
+{
+    return (std::strcmp(a, b) > 0 ? a : b);
+}
+
 template <typename T0, typename T1>
 T0 Sum(T0 a, T1 b)
 {
     return a + b;
 }
-
 
 int main()
 {    
@@ -22,4 +29,8 @@ int main()
 
     std::cout << Sum(a, d) << std::endl;
     std::cout << Sum<double>(a, b) << std::endl;
+
+    const char* A{"A"};
+    const char* B{"B"};
+    std::cout << Maximum(A, B) << std::endl;
 }

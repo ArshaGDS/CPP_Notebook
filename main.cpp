20c5f20222894a6d9ed26e3285dad39b37a2c32b
Alireza <mrarshax@gmail.com>
2023-01-04 23:01:38 +0330

    Type traits

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
+#include <type_traits>
+
+template<typename T>
+void PrintNumber(T n)
+{
+    static_assert(std::is_integral_v<T>, "PrintNumber can only be called with integral types");
+    std::cout << n << std::endl;
+}
+
 
 int main()
 {
-    auto func = [](auto A, auto B) {
-        return A + B;
-    };
-
-    // C++20
-    auto funcV20 = []<typename T0, typename T1>(T0 A, T1 B) {
-        return A + B;
-    };
-
-    int a{20};
-    double b{20.5};
-
-    std::cout << "Sum: " << func(a, b) << std::endl;
-    std::cout << "Sum: " << funcV20(a, b) << std::endl;
+   PrintNumber(5);
+   // PrintNumber(5.5); Compile error
 }

94907b45bf08a4a6a9f0786e077bc7a618d9f865
Alireza <mrarshax@gmail.com>
2023-01-05 13:09:03 +0330

    constexpr if

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <type_traits>
 
+#define cout std::cout
+#define endl std::endl
+
 template<typename T>
-void PrintNumber(T n)
+void func(T n)
 {
-    static_assert(std::is_integral_v<T>, "PrintNumber can only be called with integral types");
-    std::cout << n << std::endl;
+   if constexpr (std::is_integral_v<T>)
+   {
+        cout << "Integral" << endl;
+   }
+   else if constexpr (std::is_floating_point_v<T>)
+   {
+        cout << "Foating point" << endl;        
+   }
+   else
+   {
+       static_assert(std::is_integral_v<T> || std::is_floating_point_v<T>, "Argument must be integral or floating point.");                      
+   }
 }
 
 
 int main()
 {
-   PrintNumber(5);
-   // PrintNumber(5.5); Compile error
+    //func(2);
+    func(2.5);
 }
